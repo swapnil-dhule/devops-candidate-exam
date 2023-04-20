424bf52cafcc2c87525db64b659925e3d5c8604e
swapnil-dhule <swapneel.dhule@gmail.com>
2023-04-20 18:08:14 +0530

    main.tf updated

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "private_subnet" {
+  cidr_block        = "10.0.1.0/24"
+  vpc_id            = data.aws_vpc.vpc.id
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = data.aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = data.aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "private_route_table_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
