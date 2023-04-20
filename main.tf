e953b83dfb9b16c8dd3490bfa59fb2ca81250827
swapnil-dhule <swapneel.dhule@gmail.com>
2023-04-20 18:17:29 +0530

    cidr changed

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "private_subnet" {
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.2.0/24"
   vpc_id            = data.aws_vpc.vpc.id
   availability_zone = "ap-south-1a"
 }
