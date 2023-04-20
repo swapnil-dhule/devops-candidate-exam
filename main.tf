d0eb0380f582ac6520f4813ffad1c83d55321962
swapnil-dhule <swapneel.dhule@gmail.com>
2023-04-20 20:06:42 +0530

    bugfix

--- main.tf
+++ main.tf
@@ -19,50 +19,24 @@ resource "aws_route_table_association" "private_route_table_association" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
-resource "aws_lambda_function" "lambda" {
-  function_name = "lambda_function"
-  role         = data.aws_iam_role.lambda.arn
-  handler      = "lambda_function.lambda_handler"
-  runtime      = "python3.8"
-  timeout      = 10
-  memory_size  = 128
-  filename     = "${path.module}/lambda_function.zip"
+resource "aws_lambda_function" "example_lambda" {
+  function_name = "example_lambda_function"
+  handler = "main.handler"
+  runtime = "python3.9"
+  role = data.aws_iam_role.lambda.arn
+  filename = "${path.module}/function.zip"
+  source_code_hash = filebase64sha256("${path.module}/function.zip")
+  
+  environment {
+    variables = {
+      URL = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
+      AUTH = "test"
+    }
+  }
 }
 
-# data "archive_file" "lambda" {
-#   type        = "zip"
-#   output_path = "${path.module}/lambda_function_new.zip"
-#   source {
-#     content  = <<-EOF
-#         import requests
-#         import base64
-
-#         def lambda_handler(event, context):
-#             url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
-#             headers = {"X-Siemens-Auth": "test"}
-#             payload = {
-#                 "subnet_id": "10.0.250.0/24",
-#                 "name": "Swapnil_Dhule",
-#                 "email": "swapneel.dhule@gmail.com"
-#             }
-
-#             response = requests.post(url, headers=headers, json=payload)
-
-#             return {
-#                 "StatusCode": response.status_code,
-#                 "LogResult": base64.b64encode(response.content).decode('utf-8'),
-#                 "ExecutedVersion": "$LATEST"
-#             }
-#     EOF
-#     filename = "lambda_function.py"
-#   }
-# }
-
-
-output "lambda_function_response" {
-  value = jsonencode({
-    StatusCode     = "$context.responseStatus",
-    LogResult       = "$context.logStreamName",
-    ExecutedVersion = "$context.functionVersion",
-  })
+data "archive_file" "function_zip" {
+  type        = "zip"
+  source_file = "main.py"
+  output_path = "${path.module}/function.zip"
 }
