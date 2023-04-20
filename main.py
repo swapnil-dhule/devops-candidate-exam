2182a70a690cc9f6b0a2693b091e545460bf22c3
swapnil-dhule <swapneel.dhule@gmail.com>
2023-04-20 20:34:00 +0530

    bug

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ def lambda_handler(event, context):
         "name": "Swapnil_Dhule",
         "email": "swapneel.dhule@gmail.com"
     }
-    response = requests.post(url, headers=headers, json=payload)
-    result = {
-        "StatusCode": response.status_code,
-        "LogResult": base64.b64encode(response.content).decode('utf-8'),
-        "ExecutedVersion": "$LATEST"
+    
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        response.raise_for_status() # Raise an exception for any HTTP error status codes
+        result = {
+            "StatusCode": response.status_code,
+            "LogResult": base64.b64encode(response.content).decode('utf-8'),
+            "ExecutedVersion": "$LATEST"
+        }
+    except requests.exceptions.RequestException as e:
+        # Handle any HTTP request exceptions and return an appropriate response
+        result = {
+            "StatusCode": 500,
+            "LogResult": str(e),
+            "ExecutedVersion": "$LATEST"
         }
 
     return result
