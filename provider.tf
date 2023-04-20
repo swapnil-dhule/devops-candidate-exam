db54ed2dafe72b14a381f4bae7b04062a6a570d4
swapnil-dhule <swapneel.dhule@gmail.com>
2023-04-20 17:07:56 +0530

    provider added

--- provider.tf
+++ provider.tf
@@ -3,3 +3,10 @@ provider "aws" {
 }
 
 # Add your S3 backend configuration here
+terraform {
+  backend "s3" {
+    bucket = "3.devops.candidate.exam"
+    key    = "Swapnil.Dhule"
+    region = "ap-south-1"
+  }
+}
